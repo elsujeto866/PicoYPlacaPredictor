2e9806967738714dbb154323e08fab1b2bd1887d
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 11:17:45 -0500

    refactor: finalize CompositeCirculationValidator to use exceptions and Pico y Placa rules separation

---
+++ PicoYPlacaPredictor/Business/CompositeCirculationValidator.cs
@@ -0,0 +1,51 @@
+﻿using PicoYPlacaPredictor.Business.Interfaces;
+using PicoYPlacaPredictor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicoYPlacaPredictor.Business.Rules;
+
+namespace PicoYPlacaPredictor.Business.Validators
+{
+    public class CompositeCirculationValidator : ICirculationValidator
+    {
+        private readonly IEnumerable<ICirculationValidator> _exemptions;
+        private readonly IEnumerable<ICirculationValidator> _rules;
+
+        // Constructor por defecto que inicializa todo el sistema
+        public CompositeCirculationValidator()
+        {
+            _exemptions = new List<ICirculationValidator>
+            {
+                new WeekendValidator(),
+                new HolidayValidator()
+            };
+
+            _rules = new List<ICirculationValidator>
+            {
+                new PicoYPlacaRules()
+            };
+        }
+
+       
+        public CompositeCirculationValidator(
+            IEnumerable<ICirculationValidator> exemptions,
+            IEnumerable<ICirculationValidator> rules)
+        {
+            _exemptions = exemptions;
+            _rules = rules;
+        }
+
+        public bool CanCirculate(Vehicle vehicle)
+        {
+            // 1. If the vehicle is exempted, it can circulate
+            if (_exemptions.Any(e => e.CanCirculate(vehicle)))
+                return true;
+
+            // 2. If the vehicle is not exempted, check the rules
+            return _rules.All(r => r.CanCirculate(vehicle));
+        }
+    }
+}
---
+++ PicoYPlacaPredictor/Business/Rules/PicoYPlacaRules.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicoYPlacaPredictor.Business.Interfaces;
+using PicoYPlacaPredictor.Business.Validators;
+using PicoYPlacaPredictor.Entities;
+
+namespace PicoYPlacaPredictor.Business.Rules
+{
+    public class PicoYPlacaRules : ICirculationValidator
+    {
+        private readonly ExemptionByDigitValidator _digitValidator = new();
+        private readonly TimeRestrictionValidator _timeValidator = new();
+
+        public bool CanCirculate(Vehicle vehicle)
+        {
+
+            bool isRestrictedDigit = !_digitValidator.CanCirculate(vehicle);
+            bool isRestrictedTime = !_timeValidator.CanCirculate(vehicle);
+
+            // If the vehicle is restricted by digit and time, it cannot circulate
+            return !(isRestrictedDigit && isRestrictedTime);
+        }
+
+       
+    }
+}
