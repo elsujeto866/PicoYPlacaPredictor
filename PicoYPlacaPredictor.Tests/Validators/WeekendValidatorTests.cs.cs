f94c409af617f008fa2a505ea0b518ee415ea250
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 09:13:39 -0500

    test: add unit tests for WeekendValidator (Saturday, Sunday, weekday cases)

---
+++ PicoYPlacaPredictor.Tests/Validators/WeekendValidatorTests.cs.cs
@@ -0,0 +1,37 @@
+using PicoYPlacaPredictor.Business.Validators;
+using PicoYPlacaPredictor.Entities;
+
+namespace PicoYPlacaPredictor.Tests.Validators
+{
+    public class WeekendValidatorTests
+    {
+        private WeekendValidator _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new WeekendValidator();
+        }
+
+        [Test]
+        public void Should_Allow_Circulation_On_Saturday()
+        {
+            var vehicle = new Vehicle("ABC-1234", new DateTime(2025, 5, 10), new TimeSpan(10, 0, 0)); // Saturday
+            Assert.IsTrue(_validator.CanCirculate(vehicle));
+        }
+
+        [Test]
+        public void Should_Allow_Circulation_On_Sunday()
+        {
+            var vehicle = new Vehicle("ABC-1234", new DateTime(2025, 5, 11), new TimeSpan(10, 0, 0)); // Sunday
+            Assert.IsTrue(_validator.CanCirculate(vehicle));
+        }
+
+        [Test]
+        public void Should_Not_Apply_On_Weekdays()
+        {
+            var vehicle = new Vehicle("ABC-1234", new DateTime(2025, 5, 9), new TimeSpan(10, 0, 0)); // Friday
+            Assert.IsFalse(_validator.CanCirculate(vehicle));
+        }
+    }
+}
