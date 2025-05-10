82dce1e7003772fbe9df168eda1bc45f5982c5e9
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 08:01:48 -0500

    feat: define ITimeRestrictionContextProvider interface for dynamic time logic

---
+++ PicoYPlacaPredictor/Business/Interfaces/ITimeRestrictionContextProvider.cs
@@ -0,0 +1,14 @@
+﻿using PicoYPlacaPredictor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYPlacaPredictor.Business.Interfaces
+{
+    public interface ITimeRestrictionContextProvider
+    {
+        TimeRestrictionPeriod GetRestrictionPeriod(DateTime date);
+    }
+}
