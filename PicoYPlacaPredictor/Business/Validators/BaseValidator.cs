7157006017b08319c4c72f8056fdcc26a58d9b71
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 07:45:09 -0500

    feat: add BaseValidator with reusable time restriction logic

--- PicoYPlacaPredictor/Business/Validators/BaseValidator.cs
+++ PicoYPlacaPredictor/Business/Validators/BaseValidator.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PicoYPlacaPredictor.Business.Interfaces;
 
 namespace PicoYPlacaPredictor.Business
 {
-    public abstract class BaseValidator
+    public abstract class BaseValidator : ICirculationValidator
     {
         public abstract bool CanCirculate(Vehicle vehicle);
 
