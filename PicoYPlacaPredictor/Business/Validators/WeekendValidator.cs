04f83a15030baff536667a213004319d684e52f5
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 07:22:04 -0500

    feat: add WeekendValidator to allow circulation on weekends

---
+++ PicoYPlacaPredictor/Business/Validators/WeekendValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicoYPlacaPredictor.Entities;
+using PicoYPlacaPredictor.Business.Interfaces;
+
+namespace PicoYPlacaPredictor.Business.Validators
+{
+    public class WeekendValidator : ICirculationValidator
+    {
+        public bool CanCirculate(Vehicle vehicle)
+        {
+            var day = vehicle.Date.DayOfWeek;
+            return day == DayOfWeek.Saturday || day == DayOfWeek.Sunday;
+        }
+    }
+
+
+}
+
