472e996d20be2ea52f2da31f70e5f4bdfa43269a
Elsujeto <doriandavidmza@gmail.com>
2025-05-09 22:07:44 -0500

    feat: add Vehicle entity with license plate, date and time

---
+++ PicoYPlacaPredictor/Entities/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYPlacaPredictor.Entities
+{
+    class Vehicle
+    {
+        public string LicensePlate { get; }
+        public DateTime Date { get; }
+        public TimeSpan Time{ get; }
+
+        public Vehicle(string licensePlate, DateTime date, TimeSpan time)
+        {          
+            LicensePlate = licensePlate;
+            Date = date;
+            Time = time;
+        }
+    }
+}
