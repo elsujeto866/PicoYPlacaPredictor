5bd8eeac00d5ef5e2afc7181beaf922409273261
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 08:54:17 -0500

    feat: use CompositeCirculationValidator with fixed time provider in main validation flow

---
+++ PicoYPlacaPredictor/Business/Validators/CompositeCirculationValidator.cs
@@ -0,0 +1,25 @@
+﻿using PicoYPlacaPredictor.Business.Interfaces;
+using PicoYPlacaPredictor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYPlacaPredictor.Business.Validators
+{
+    public class CompositeCirculationValidator :BaseValidator
+    {
+        private readonly IEnumerable<ICirculationValidator> _validators;
+
+        public CompositeCirculationValidator(IEnumerable<ICirculationValidator> validators)
+        {
+            _validators = validators;
+        }
+
+        public override bool CanCirculate(Vehicle vehicle)
+        {
+            return _validators.All(v => v.CanCirculate(vehicle));
+        }
+    }
+}
