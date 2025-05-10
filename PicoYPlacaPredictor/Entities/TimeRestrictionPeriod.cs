e5385277866d5e808bbd51119c65444c0bc86ec0
Elsujeto <doriandavidmza@gmail.com>
2025-05-10 08:00:20 -0500

    feat: define TimeRestrictionPeriod record to encapsulate morning and evening restriction hours

---
+++ PicoYPlacaPredictor/Entities/TimeRestrictionPeriod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoYPlacaPredictor.Entities
+{ 
+    public record TimeRestrictionPeriod(
+        (TimeSpan Start, TimeSpan End) Morning,
+        (TimeSpan Start, TimeSpan End) Evening);
+   
+}
