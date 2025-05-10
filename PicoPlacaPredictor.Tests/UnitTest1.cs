8904e82307c20a66a4ae120513506ce8d6baef6e
Elsujeto <doriandavidmza@gmail.com>
2025-05-09 21:01:02 -0500

    chore: create folders for Model, View, ViewModel, and Tests

---
+++ PicoPlacaPredictor.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PicoPlacaPredictor.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
